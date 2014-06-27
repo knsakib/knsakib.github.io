b0f8479256dcc64735795486fbbf92597223130e
Khalid Nazmus Sakib <knsakib@gmail.com>
2014-06-27 02:58:19 -0400

    Update instant_update.php

--- guestbook/instant_update.php
+++ guestbook/instant_update.php
@@ -1,10 +1,10 @@
 <?php
 
-if (mysql_connect('fdb2.biz.nf','1470566_post','rootpass') && mysql_select_db('1470566_post')){
+if (mysql_connect('ec2-54-187-216-239.us-west-2.compute.amazonaws.com','root','HssM4F') && mysql_select_db('messages')){
 // if mysql connected and mysql database guestbook connected
 // @ sign before 'mysql_connect' and 'mysql_select_db' force not to show default error message
     
-    $entries = mysql_query("SELECT * FROM entries ORDER BY `timestamp` DESC LIMIT 1");
+    $entries = mysql_query("SELECT * FROM messages ORDER BY `timestamp` DESC LIMIT 1");
     //$entries = mysql_query("SELECT `timestamp`, `name`, `email`,`message` FROM entries ORDER BY `timestamp` DESC LIMIT 1");
     //$entries_row = mysql_query($entries);
     $entries_row=mysql_fetch_array($entries);       
